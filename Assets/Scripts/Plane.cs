b7d962154c5e2c3cf6691fd11b50ab7a297468da
Matthew Onghai <matthewonghai@utexas.edu>
2020-01-01 21:18:05 -0600

    Added game over and basic menu functionality

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -58,10 +58,13 @@ public class Plane : MonoBehaviour
         planeTransform.localScale = new Vector3(-8, 8, 8);
     }
 
+    // called when first tap is registered
     private void StartMovement(){
+        Level.GetInstance().StartLevel();
         planeBody.velocity = new Vector2(-1 * MOVE_AMOUNT, 0);
     }
 
+    // game over trigger
     private void OnTriggerEnter2D(Collider2D collision){
         planeBody.velocity = new Vector2(0f, 0f);
         planeBody.gravityScale = 5f;
@@ -70,3 +73,8 @@ public class Plane : MonoBehaviour
     }
 
 }
+
+
+
+// to reload game scene
+// UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
